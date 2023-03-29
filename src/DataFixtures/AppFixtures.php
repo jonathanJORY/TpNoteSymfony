0a850ed7c4bdce2b68bfcc81e96bc7f43487623a
CHARPENTIER Maxym <o22103433@info07-17.iut45.univ-orleans.fr>
2023-03-29 15:28:59 +0200

    avancelenbté

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -16,49 +16,43 @@ use SpotifyWebAPI\SpotifyWebAPI;
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
-    {
-        // Créer une session Spotify
-        $session = new Session(
-            'ea11311d67e048869c710972a24e46be',
-            '358d7f3fdc2e4353b8b4e9e0af646faf',
-            'localhost:3000'
-        );
-        
-        // Obtenir un jeton d'accès
-        $session->requestCredentialsToken();
-        $accessToken = $session->getAccessToken();
-        
-        // Créer une instance de l'API Spotify
-        $api = new SpotifyWebAPI();
-        $api->setAccessToken($accessToken);
-        
-        // Récupérer les albums de l'artiste "Ed Sheeran"
-        $albums = $api->getArtistAlbums('6eUKZXaKkcviH0Ku9w2n3V', ['album_type' => 'album']);
-        
-        // Parcourir les albums
+{
+    // Créer une session Spotify
+    $session = new Session(
+        'ea11311d67e048869c710972a24e46be',
+        '358d7f3fdc2e4353b8b4e9e0af646faf',
+        'localhost:3000'
+    );
+
+    // Obtenir un jeton d'accès
+    $session->requestCredentialsToken();
+    $accessToken = $session->getAccessToken();
+
+    // Créer une instance de l'API Spotify
+    $api = new SpotifyWebAPI();
+    $api->setAccessToken($accessToken);
+
+    $artists = $api->getArtists([
+        'ids' => ['6eUKZXaKkcviH0Ku9w2n3V', '6l3HvQ5sa6mXTsMTB19rO5'] // Exemple avec Ed Sheeran et Coldplay
+    ])->artists;
+
+    // Parcourir les artistes
+    foreach ($artists as $artistData) {
+        // Créer un nouvel artiste
+        $artist = new Artist();
+        $artist->setName($artistData->name);
+
+        // Ajouter les albums de l'artiste
+        $albums = $api->getArtistAlbums($artistData->id, ['album_type' => 'album']);
         foreach ($albums->items as $albumData) {
             // Créer un nouvel album
             $album = new Album();
             $album->setName($albumData->name);
             $album->setReleaseDate($albumData->release_date);
             $album->setImage($albumData->images[0]->url);
-            
-            // Ajouter les artistes de l'album
-            foreach ($albumData->artists as $artistData) {
-                // Vérifier si l'artiste existe déjà dans la base de données
-                $artist = $manager->getRepository(Artist::class)->findOneBy(['name' => $artistData->name]);
-                
-                // S'il n'existe pas, le créer
-                if (!$artist) {
-                    $artist = new Artist();
-                    $artist->setName($artistData->name);
-                    $manager->persist($artist);
-                }
-                
-                $album->addArtist($artist);
-            }
-            
-            // Ajouter les genres de l'album
+
+            $album->addArtist($artist);
+
             // Ajouter les genres de l'album
             if (property_exists($albumData, 'genres')) {
                 foreach ($albumData->genres as $genreData) {
@@ -76,23 +70,25 @@ class AppFixtures extends Fixture
                 }
             }
 
-            
             // Ajouter les pistes de l'album
             $tracks = $api->getAlbumTracks($albumData->id);
-            
             foreach ($tracks->items as $trackData) {
                 $track = new Track();
                 $track->setName($trackData->name);
-                $track->setImage($albumData->images[0]->url);
+                $track->setDuration($trackData->duration_ms);
+                $track->setPreviewUrl($trackData->preview_url);
                 $track->setAlbum($album);
-                
+
                 $manager->persist($track);
             }
-            
+
             $manager->persist($album);
         }
-        
-        $manager->flush();
+
+        $manager->persist($artist);
     }
+
+    $manager->flush();
+}
 }
 
